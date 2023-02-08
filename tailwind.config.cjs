42c557222b4d14fabb92751e7439901947b41681
dvqc-office <bra-95@live.fr>
2023-02-08 15:37:47 +0100

    added a badge component

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      minWidth: {
+        72: "18rem",
+      },
+      scale: {
+        102: "102%",
+      },
+    },
+  },
+  plugins: [],
+};
