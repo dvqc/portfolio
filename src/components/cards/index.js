f3b0b3c3e031d5838daf348e1137e9cbb875a5a0
Brahim <bra-95@live.fr>
2023-02-10 23:25:19 +0100

    fixed tech badges spacing, centered card titles

---
+++ src/components/cards/index.js
@@ -0,0 +1,4 @@
+export { default as Card } from "./Card.astro";
+export { default as CardImage } from "./CardImage.tsx";
+export { default as CardsContainer } from "./CardsContainer.astro";
+export { default as LinkCard } from "./LinkCard.astro";
--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,5 +1,6 @@
-// export { default as LinkItem } from "./LinkItem.astro";
-// export { default as Links } from "./Links.astro";
-// export { default as Nav } from "./Nav.astro";
-// export { default as NavItem } from "./NavItem.astro";
+export { default as LinkItem } from "./LinkItem.astro";
+export { default as Links } from "./Links.astro";
+export { default as Nav } from "./Nav.astro";
+export { default as NavItem } from "./NavItem.astro";
 export { default as Sidebar } from "./Sidebar.tsx";
+export { default as Topbar } from "./Topbar.tsx";
